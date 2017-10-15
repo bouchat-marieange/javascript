83b72dfb52128b04d017df9fa5769f16bcedd649
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-16 00:12:02 +0200

    Exercice partie 3 - Fonctions terminé

--- 3-FONCTIONS/app.js
+++ 3-FONCTIONS/app.js
@@ -1,3 +1,43 @@
-// Exercice 
+// Exercice Fonctions
 
+var soustraction = function (a, b)
+{
+  return a - b;
+}
 
+var division = function (a, b)
+{
+  return a / b;
+}
+
+var multiplication = function (a, b)
+{
+  return a * b;
+}
+
+var pourcentage = function (a, b)
+{
+  return b / a;
+}
+
+var vitesse = function (a, b)
+{
+  return a / b;
+}
+
+// Affichage résultat des différentes Fonctions
+
+var resultat = soustraction (200, 2);
+alert ("La différence est de " + resultat);
+
+var resultat = division (200, 2);
+alert ("La quotient est de " + resultat);
+
+var resultat = multiplication(200, 2);
+alert ("Le produit est de " + resultat);
+
+var resultat = vitesse (50, 10);
+alert ("Le pourcentage de ce nombre vaut " + resultat);
+
+var resultat = vitesse (200, 2);
+alert ("J'ai roulé à " + resultat + "km/h");
--- 3-FONCTIONS/essai_readme.js
+++ 3-FONCTIONS/essai_readme.js
@@ -1,3 +1,22 @@
 // Bac à sable pour s'entrainer en lisant le readme
 
 
+// Déclaration d'une fonction dont le résultat sera stocké dans la variable add
+// Pour déclarer une fonction utiliser le mot function et lister ses arguments (les éléments dont elle a besoin) entre les parenthèses
+//Ici a et b sont les arguments de la fonction
+//Ecriver ensuite entre parenthèse le code de fonction (ce qu'elle fait). Dans ce cas, elle fait l'addition de son premier argument et du second.
+//return signifie que l'on veut obtenir le resultat de la fonction (ce qu'elle renvoie)
+//return arrête également l'éxécution du code de la fonction. Après return rien ne sera exécuté.
+var add= function (a, b)
+{
+  return a + b;
+}
+
+// Une fois la fonction déclarée, on peut l'appeler et stocké le résultat qu'elle renvoie dans une variable
+// var result est la variable que l'on utilise pour stocker le résultat
+// add est le nom de la fonction que l'on appelle simplement en tapant son nom
+// 1 et 2 sont les arguments nécessaires à la fonction (qui va les additionner).
+// A l'intérieur de la fonction 1 et 2 seront enregistrer dans les variables a et b.
+// la console affiche le résultat renvoyer par la fonction et stockée dans la variable result
+var result = add (1, 2);
+-> result est égal a 3

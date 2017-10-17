5ac5c0f75d44557fe49697ccf312cdcb8138e248
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-17 13:55:17 +0200

    Exo tableaux presque fini

--- 05-TABLEAUX/app.js
+++ 05-TABLEAUX/app.js
@@ -50,3 +50,85 @@ VM440:3 8 + 9 =
 VM440:4 17
 
 // Vos meilleurs choix
+
+let acteur = ["Hanks", "Smith", "Williams"]
+for (let i = 0; i < acteur.length; i ++)
+{
+  let position = i + 1;
+  console.log ("Le numéro " + position + " est " + acteur[i]);
+}
+
+// Résultat
+// Le numéro 1 est Hanks
+// Le numéro 2 est Smith
+// Le numéro 3 est Williams
+
+// Vos meilleurs choix Bonus
+
+let acteur = ["Hanks", "Smith", "Williams"]
+let position;
+let test;
+for (let i = 0; i < acteur.length; i ++)
+{
+  position = i + 1;
+
+    if (position == 1)
+    {
+    	test = "premier";
+    }
+    else if (position == 2)
+    {
+      test = "deuxieme";
+    }
+    else if (position == 3)
+    {
+    	test = "troisième";
+    }
+    else
+    {
+      test = "plus dans le top 3";
+    }
+
+  console.log ("Le " + test + " est " + acteur[i]);
+}
+
+//Vos meilleurs choix Bonus mais avec un switch plutot qu'avec un if... elseif... else...
+let acteur = ["Hanks", "Smith", "Williams"]
+let position;
+let test;
+for (let i = 0; i < acteur.length; i ++)
+{
+  position = i + 1;
+
+  switch(position)
+  {
+  	case 1 :
+  	    test = "premier";
+  	    break;
+  	case 2 :
+  	    test = "deuxième";
+  	    break;
+  	case 3 :
+  	    test = "troisième";
+  	    break;
+  	default :
+  	    test = "plus dans le top 3";
+  	    break;
+  }
+
+  console.log ("Le " + test + " est " + acteur[i]);
+}
+
+// Résultat
+// Le premier est Hanks
+// Le deuxième est Smith
+// Le troisième est Williams
+
+
+
+//Clone
+
+let perso_mariokart = ["Mario","Luigi", "Peach"]
+let perso_mariokart_clone = perso_mariokart(1,2,3);
+// Si on veut cloner la totalité d'un tableau il n'est même pas nécessaire d'indiquer de chiffre entre les parenthèses.
+// En les laissant vides, cela clone automatiquement la totalité du tableaux

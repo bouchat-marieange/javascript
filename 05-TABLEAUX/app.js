5719d205b19a8054be989e09702fc742ac1a2350
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-18 09:52:10 +0200

    Exercice partie 5 Tableaux terminés

--- 05-TABLEAUX/app.js
+++ 05-TABLEAUX/app.js
@@ -3,7 +3,7 @@
 // Addition
 
 let chiffre = [1, 2, 3, 4, 5, 6, 7 ,8, 9];
-for (let i =0 ; i < chiffres.length; i++)
+for (let i =0 ; i < chiffre.length; i++)
 {
   console.log(chiffre[i]);
   console.log(chiffre[i]+ i);
@@ -15,39 +15,44 @@ for (let j =1 ; j < 10; j++)
 {
   console.log(j);
 }
-VM347:3 1
-VM347:3 2
-VM347:3 3
-VM347:3 4
-VM347:3 5
-VM347:3 6
-VM347:3 7
-VM347:3 8
-VM347:3 9
+
+// Résultat:
+// VM347:3 1
+// VM347:3 2
+// VM347:3 3
+// VM347:3 4
+// VM347:3 5
+// VM347:3 6
+// VM347:3 7
+// VM347:3 8
+// VM347:3 9
 
 // Boucle pour afficher l'addition et en dessous le résultat de l'addition
-let chiffre = [1, 2, 3, 4, 5, 6, 7 ,8, 9];
+let chiffre1 = [1, 2, 3, 4, 5, 6, 7 ,8, 9];
 for (let j =1 ; j <=8; j++)
 {
-  console.log (j + " + "+ chiffre[j] + " = ");
-  console.log(chiffre[j]+ j);
+  console.log (j + " + "+ chiffre1[j] + " = ");
+  console.log(chiffre1[j]+ j);
 }
-VM440:3 1 + 2 =
-VM440:4 3
-VM440:3 2 + 3 =
-VM440:4 5
-VM440:3 3 + 4 =
-VM440:4 7
-VM440:3 4 + 5 =
-VM440:4 9
-VM440:3 5 + 6 =
-VM440:4 11
-VM440:3 6 + 7 =
-VM440:4 13
-VM440:3 7 + 8 =
-VM440:4 15
-VM440:3 8 + 9 =
-VM440:4 17
+
+
+// Résultat:
+// VM440:3 1 + 2 =
+// VM440:4 3
+// VM440:3 2 + 3 =
+// VM440:4 5
+// VM440:3 3 + 4 =
+// VM440:4 7
+// VM440:3 4 + 5 =
+// VM440:4 9
+// VM440:3 5 + 6 =
+// VM440:4 11
+// VM440:3 6 + 7 =
+// VM440:4 13
+// VM440:3 7 + 8 =
+// VM440:4 15
+// VM440:3 8 + 9 =
+// VM440:4 17
 
 // Vos meilleurs choix
 
@@ -65,7 +70,7 @@ for (let i = 0; i < acteur.length; i ++)
 
 // Vos meilleurs choix Bonus
 
-let acteur = ["Hanks", "Smith", "Williams"]
+let acteur1 = ["Hanks", "Smith", "Williams"]
 let position;
 let test;
 for (let i = 0; i < acteur.length; i ++)
@@ -89,34 +94,34 @@ for (let i = 0; i < acteur.length; i ++)
       test = "plus dans le top 3";
     }
 
-  console.log ("Le " + test + " est " + acteur[i]);
+  console.log ("Le " + test + " est " + acteur1[i]);
 }
 
 //Vos meilleurs choix Bonus mais avec un switch plutot qu'avec un if... elseif... else...
-let acteur = ["Hanks", "Smith", "Williams"]
-let position;
-let test;
+let acteur2 = ["Hanks", "Smith", "Williams"]
+// let position;
+let test2;
 for (let i = 0; i < acteur.length; i ++)
 {
-  position = i + 1;
+  let position = i + 1;
 
   switch(position)
   {
   	case 1 :
-  	    test = "premier";
+  	    test2 = "premier";
   	    break;
   	case 2 :
-  	    test = "deuxième";
+  	    test2 = "deuxième";
   	    break;
   	case 3 :
-  	    test = "troisième";
+  	    test2 = "troisième";
   	    break;
   	default :
-  	    test = "plus dans le top 3";
+  	    test2 = "plus dans le top 3";
   	    break;
   }
 
-  console.log ("Le " + test + " est " + acteur[i]);
+  console.log ("Le " + test2 + " est " + acteur2[i]);
 }
 
 // Résultat
@@ -128,7 +133,20 @@ for (let i = 0; i < acteur.length; i ++)
 
 //Clone
 
-let perso_mariokart = ["Mario","Luigi", "Peach"]
-let perso_mariokart_clone = perso_mariokart(1,2,3);
 // Si on veut cloner la totalité d'un tableau il n'est même pas nécessaire d'indiquer de chiffre entre les parenthèses.
-// En les laissant vides, cela clone automatiquement la totalité du tableaux
+// En les laissant vides, cela clone automatiquement la totalité de toutes les cellules du tableaux
+// Ajout d'un nouveau personnage (Bowser) à la fin du tableau cloné avec push
+let mariokart = ["Mario","Luigi", "Peach"];
+// undefined
+let mariokart_clone = perso_mariokart.slice();
+// undefined
+mariokart_clone[0];
+// "Mario"
+mariokart_clone[1];
+// "Luigi"
+mariokart_clone[2];
+// "Peach"
+mariokart_clone.push('Bowser');
+// 4
+mariokart_clone[3];
+// "Bowser"

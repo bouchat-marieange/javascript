bd3271b809c13d8e008a9e5f3f2005cd456b5fa8
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-20 10:23:27 +0200

    Premier exercice de la partie 8 Objets terminé - fonction dans objet, usage de this, élimination du code superflu et commentaires ok

--- 08-OBJETS/app.js
+++ 08-OBJETS/app.js
@@ -14,52 +14,46 @@
 //Création de l'objet
 Character =
 {
-  "name":"John",
-  "age":30,
-  "items_to_give":
-    {
-      "item1":"objet inconnu",
-      // "item2":"Objet secret"
-    }
+  name:"John",
+  age:30,
+  items_to_give:["épée", "bouclier", "casque", "massue", "hache", "arbalète"],
+  giveItem:  function(){
+    var rand_index = Math.random()*(this.items_to_give.length);
+    rand_index = Math.floor (rand_index);
+
+    return this.items_to_give[rand_index];
+  }
 }
 
+// Fonction giveItem pour choisir aléatoirement un objet dans le tableau item_to_give imbriqué dans l'objet Character
+// On va placer la fonction directement dans l'objet sous le tableau.
+// Ne pas oublier d'ajouter this. lorsque l'on fait référence au tableau cité plus haut,comme ça l'ordi comprend qui doit aller vérifier dans tout l'objet que l'élément auquel on fait référence à bien été déclaré
+
+// Pour attribuer un objet aléatoirement, on créer un tableau avec des objets
+// On crée une variable rand_index qui va générer un numéro d'index aléatoire avec Math.random et lui envoyer les paramètre du tableau (0 et 5) - la valeur minimal et maximal d'index du tableau item
+// On modifie la variable rand_index pour l'arrondir à un nombre entier avec Math.floor
+// La fonction giveItem va renvoyer la valeur correspondant à l'index aléatoire stocké dans la variable rand_index
+
 //Affichage des propriétés de l'objet (string, numero et tableau) - 1 propriété par ligne dans console
+//
 var sous_tableau = Character.items_to_give;
 for (x in Character)
 {
-  console.log (Character[x])
-  // console.log(this.items_to_give);
-  // document.getElementById("demo").innerHTML += Character[x] + "<br>";
-
-  // for (var i=0; i<sous_tableau.length;i++)
-  //   {
-  //     console.log(Character.items_to_give[i]);
-  //   }
-}
-  console.log (Character.items_to_give.item1);
-
-
-
-
-// Fonction giveItem pour choisir aléatoirement un objet qui va remplacer l'item1 dans les propriété de l'objet Character
-var item = ["épée", "bouclier", "casque", "massue", "hache", "arbalète"];
-
-var rand_index = Math.random()*(item.length - 0);
-var rand_index = Math.floor (rand_index) + 0;
-console.log (rand_index);
-var objet_aleatoire = item[rand_index];
-console.log (objet_aleatoire);
-
-function giveItem()
-{
-  Character.items_to_give.item1 = objet_aleatoire;
+  if(x != "items_to_give" && x != "giveItem"){
+    console.log (Character[x]);
+  }
+  else if(x =="giveItem"){
+    var item = Character.giveItem();
+    console.log (item);
+  }
 }
 
-giveItem();
-
-// Pour attribuer un objet aléatoirement, on créer un tableau avec des objets
-// On crée ensuite générer un numéro d'index aléatoire avec Math.random et lui envoyer les paramètre du tableau (0 et 5) - la valeur minimal et maximal d'index du tableau item
-// et attribuer l'objet correspondant à cet index que l'on affichera dans la console pour vérification
-// On stockera cette valeur aléatoire dans une variable nommée objet_aleatoire
-// la valeur de cet objet devra venir remplacer celle de item1 dans l'objet Character
-// On changera cette valeur en indiquant Character.items_to_give.item1 = objet_aleatoire
+// Pour afficher dans un boucle for in à la fois les valeur simple comme (string, number, ...)
+// On utilise console.log (Character[x]); Soit le nom de l'objet et entre crochets la clé qui correspond à la valeur que l'on veut afficher.
+// Mais cette boucle affichera bien les valeur simple mais pas les tableaux imbriqué dans l'objet.
+// Il faut donc créer un condition (if elseif) indiquant comment les données simple doivent être affichées et comment les tableaux doivent être affichés
+// Dans le if on indique que l'on utilise l'affichage standard des valeur lorsque x n'est pas égale à item_to_give et n'est pas égale non plus à la fonction (car sinon il va afficher la fonction dans la console)
+// En gros on lui dit Affichage normal pour tout ce qui n'est pas tableau le tableau item_to_give et qu'on ne boucle pas non plus sur la foncion giveItem
+// Dans le elseif on indique ce qu'il faut faire si x est égale à au tableau"giveItem", donc comment afficher les tableaux imbriqué
+// Pour afficher tableau imbriqué on va stocké dans une variable apellée item l'élément récupérer Character.giveitem() . Soit la valeur de ce qui se trouve dans l'objet Character et qui correspond à la clef retournée par la fonction giveItem
+// Attention ne pas mettre Character.items_to_give car à ce moment là il va boucler sur tout les éléments du tableau items_to_give au lieu de n'afficher que celui qui a été choisi avec la fonction giveItem.

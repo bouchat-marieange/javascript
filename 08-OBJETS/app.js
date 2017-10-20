3982893eab88cb94f13b28bd74d6e3fe668a0d17
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-20 12:35:02 +0200

    Partie 8 Objet - Exercice 2 terminé mais pas encore commenté, approfondir notion for in versus for of

--- 08-OBJETS/app.js
+++ 08-OBJETS/app.js
@@ -29,6 +29,7 @@ Character =
 // On va placer la fonction directement dans l'objet sous le tableau.
 // Ne pas oublier d'ajouter this. lorsque l'on fait référence au tableau cité plus haut,comme ça l'ordi comprend qui doit aller vérifier dans tout l'objet que l'élément auquel on fait référence à bien été déclaré
 
+// Attention la syntaxe pour déclarer une fonction à l'intérieur d'un objet giveItem:  function() n'est pas la même que pour la déclaer à l'extérieur d'un objet function affiche_objets()
 // Pour attribuer un objet aléatoirement, on créer un tableau avec des objets
 // On crée une variable rand_index qui va générer un numéro d'index aléatoire avec Math.random et lui envoyer les paramètre du tableau (0 et 5) - la valeur minimal et maximal d'index du tableau item
 // On modifie la variable rand_index pour l'arrondir à un nombre entier avec Math.floor
@@ -78,26 +79,80 @@ for (x in Character)
 // faire une fonction pour afficher les objets disponibles
 // faire une fonction pour afficher les objets dont le niveau minimum est de 10
 
-objet_a_vendre =
-{
-  epee : ["épée", 8, 1, 5, true],
-  poison : ["poison", 8, 1, 5, false],
-  baton : ["baton", 8, 1, 5, true],
-  arbalete : ["arbalete", 8, 1, 5, true],
-  hache : ["hache", 8, 1, 5, true],
-  //index 0 : title
-  //index 1 : physic
-  //index 2 : magic
-  //index 3 : minLevel
-  //index 4: available
 
-  affiche_objets: function()
-  {
-    for (x in objet_a_vendre)
-    {
-      console.log (this.objet_a_vendre[x]);
-    }
-  }
+
+// un variable qui stocke chaque objet
+var epee =
+{
+  title : "épée",
+  physic : 8,
+  magic : 1,
+  minLevel : 5,
+  available: true
 }
 
+var poison =
+{
+  title : "poison",
+  physic : 1,
+  magic : 10,
+  minLevel : 1,
+  available: false
+}
+
+var baton =
+{
+  title : "baton",
+  physic : 2,
+  magic : 0,
+  minLevel : 3,
+  available: true
+}
+
+var arbalete =
+{
+  title : "arbalete",
+  physic : 5,
+  magic : 1,
+  minLevel : 4,
+  available: true
+}
+
+var hache =
+{
+  title : "hache",
+  physic : 9,
+  magic : 0,
+  minLevel : 9,
+  available: true
+}
+
+// Créer un objet tableau qui contient une variable définie avec toutes ses propriétés pour chaque arme. (epee, poisson, etc..). Celle-ci ne sont pas entre "" car il s'agit de nom de variables que l'on à crées et pas de string
+var objetAVendre = [epee,poison,baton, arbalete, hache];
+
+//Créer une fonction pour afficher tout les objets
+function affiche_objets()
+  {
+    for (x in objetAVendre)
+    {
+      console.log(objetAVendre[x].title);
+    }
+  }
+
+// Appelle la fonction affiche objets
 affiche_objets();
+
+//Créer une fonction pour afficher tout les objets disponibles (available == true)
+// x n'est pas une valeur d'incrément, c'est directement l'objet for in (renvoie la propriété d'un objet et pas la valeur) - pour for of pour les liste x c'est l'ojet lui-même (renvoie la valeur d'une liste)
+function affiche_objets_disponibles()
+  {
+    for (x of objetAVendre)
+    {
+      if (x.available == true)
+      {
+        console.log(x.title);
+      }
+    }
+  }
+
+affiche_objets_disponibles();
